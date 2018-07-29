2d92839c81ac1f026bb3af589fa177eca7083717
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-29 08:19:11 -0700

    Add menu

--- app/src/main/java/com/khahani/app/quakeeventlist/MainActivity.java
+++ app/src/main/java/com/khahani/app/quakeeventlist/MainActivity.java
@@ -5,6 +5,9 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -39,6 +42,28 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_refresh:
+                new QuakeAsyncTask().execute(EVENT_URL);
+                break;
+            case R.id.action_show:
+                Toast.makeText(this, "Hello from menu", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
